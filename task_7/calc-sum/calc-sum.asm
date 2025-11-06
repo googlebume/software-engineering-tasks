022224cbbfe74606cc5071303f7fe33f85e98237
Serhii Duzia <duzasergej@gmail.com>
2025-11-06 15:24:00 +0200

    Completed the program for calculating the amount in task_7

---
+++ task_7/calc-sum/calc-sum.asm
@@ -0,0 +1,56 @@
+; --- calc-sum ---
+; Author Duzya Serhii
+; nasm -f win64 calc-sum.asm -o calc-sum.obj
+; gcc calc-sum.obj -o calc-sum.exe
+
+section .data
+    fmt_prompt db "Enter n: ", 0
+    fmt_in db "%d", 0
+    fmt_out db "Sum = %d", 10, 0
+    n dd 0
+    sum dd 0
+
+section .text
+    global main
+    extern printf
+    extern scanf
+
+main:
+    sub rsp, 40  ; Виділити місце в стеку (shadow space + вирівнювання)
+
+    ; Вивести запрошення
+    lea rcx, [rel fmt_prompt]
+    xor eax, eax
+    call printf
+
+    ; Зчитати n
+    lea rcx, [rel fmt_in]
+    lea rdx, [rel n]
+    xor eax, eax
+    call scanf
+
+    ; Обчислити суму
+    mov ecx, dword [rel n]
+    test ecx, ecx           ; Перевірка на 0
+    jle skip_sum            ; Якщо n <= 0, пропустити цикл
+    
+    xor eax, eax
+    xor ebx, ebx
+
+sum_loop:
+    inc ebx
+    add eax, ebx
+    loop sum_loop
+
+skip_sum:
+    mov [rel sum], eax
+
+    ; Вивести результат
+    lea rcx, [rel fmt_out]
+    mov edx, eax
+    xor eax, eax
+    call printf
+
+    add rsp, 40  ; Відновити стек
+    xor eax, eax ; Повернути 0
+    ret
